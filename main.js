e24d8034922467a585aa819009fb1706fad0c82f
Colin Turing <colin.koga@gmail.com>
2019-07-27 11:30:32 -0600

    Add urgent card to style created querySelectors for inputs and button added list item to hold spce in html

--- main.js
+++ main.js
@@ -3,15 +3,27 @@ var titleInput = document.querySelector('#title-input');
 var toDoInput = document.querySelector('#task-input');
 var createPendingBtn = document.querySelector('#make-task-btn');
 var createListBtn = document.querySelector('#task-list-btn');
-
 var form = document.querySelector('form')
-form.addEventListener('keyup', disableBtns);
+
+form.addEventListener('focusout', disableBtns);
+form.addEventListener('click', plusBtnEventHandler);
+
+function plusBtnEventHandler(e) {
+  e.preventDefault();
+  if (e.target.closest('#task-list-btn') [
+    createPending();
+  ])
+}
 
 function disableBtns() {
   console.log('hi');
-  if (titleInput.value === '' && toDoInput.value === '') {
+  if (titleInput.value === '' || toDoInput.value === '') {
     createPendingBtn.disabled = true;
   } else {
     createPendingBtn.disabled = false;
   };
 };
+
+function createPending() {
+  document.querySelector('.section').insertAdjacentHTML('afterbegin', ``)
+}
